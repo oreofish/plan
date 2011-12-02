572884661fb2df056afe4391a6fd86045951e99e
Rick Xing <oreofish@gmail.com>
2011-12-02 23:29:56 +0800

    limit message:content length and UT

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id             :integer         not null, primary key
+#  user_id        :integer
+#  content        :string(255)
+#  has_attachment :boolean
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class Message < ActiveRecord::Base
+  validates :content, :length => { :maximum => 140 }
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,34 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id             :integer         not null, primary key
+#  user_id        :integer
+#  content        :string(255)
+#  has_attachment :boolean
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do 
+    @msg = { 
+      :user_id => 1,
+      :content => 'test msg',
+      :has_attachment => false
+    }
+  end
+  
+  it "should create new msg" do 
+    Message.create!(@msg)
+  end
+
+  it "should NOT create msg with too long content" do 
+    long_content = 'a' * 141
+    long_msg = Message.new(@msg.merge(:content => long_content))
+    long_msg.should_not be_valid
+  end
+  
 end
